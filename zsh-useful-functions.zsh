990d2ae4309825182bf41a66888e345aa4b77336
Andrés Merlo Trujillo <a.merlo.truji10@gmail.com>
2024-06-12 12:57:05 +0200

    Added new function

--- zsh-useful-functions.zsh
+++ zsh-useful-functions.zsh
@@ -342,3 +342,13 @@ check_binary_contents_cmp(){
 
     unset file
 }
+
+iommu_groups(){
+    # shopt -s nullglob
+    for g in $(find /sys/kernel/iommu_groups/* -maxdepth 0 -type d | sort -V); do
+        echo "IOMMU Group ${g##*/}:"
+        for d in $g/devices/*; do
+            echo -e "\t$(lspci -nns ${d##*/})"
+        done;
+    done;
+}
